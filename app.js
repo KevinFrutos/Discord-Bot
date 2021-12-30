4003e5af0115703299ac743e4b9a39d07d52e2b2
KevinFrutos <vinke.14297@gmail.com>
2021-12-30 19:30:51 +0100

    Añadiendo Prettier

--- app.js
+++ app.js
@@ -1,52 +1,52 @@
-import dotenv from 'dotenv'
-import { REST } from '@discordjs/rest'
-import { Routes } from 'discord-api-types/v9'
-import { Client, Intents, Collection } from 'discord.js'
-import fs from 'fs'
+import dotenv from "dotenv";
+import { REST } from "@discordjs/rest";
+import { Routes } from "discord-api-types/v9";
+import { Client, Intents, Collection } from "discord.js";
+import fs from "fs";
 
-dotenv.config()
+dotenv.config();
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
-const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const rest = new REST({ version: "9" }).setToken(process.env.TOKEN);
 
 client.commands = new Collection();
 const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
 (async () => {
-    // IMPORTANDO COMANDOS
-    for (const file of commandFiles) {
-        const command = await import(`./commands/${file}`)
-        commands.push(command.default.data);
-        client.commands.set(command.default.data.name, command)
-    }
-    // REGISTRANDO COMANDOS
-    try {
-        await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
-        console.log("Comandos regisrados correctamente");
-    } catch (error) {
-        console.log(error)
-    }
-})()
+	// IMPORTANDO COMANDOS
+	for (const file of commandFiles) {
+		const command = await import(`./commands/${file}`);
+		commands.push(command.default.data);
+		client.commands.set(command.default.data.name, command);
+	}
+	// REGISTRANDO COMANDOS
+	try {
+		await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands });
+		console.log("Comandos regisrados correctamente");
+	} catch (error) {
+		console.log(error);
+	}
+})();
 
-client.on('ready', async () => {
-    console.log(`Conectado como: ${client.user.tag}`);
-})
+client.on("ready", async () => {
+	console.log(`Conectado como: ${client.user.tag}`);
+});
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) {
-        return
-    }
+client.on("interactionCreate", async interaction => {
+	if (!interaction.isCommand()) {
+		return;
+	}
 
-    const command = client.commands.get(interaction.commandName);
+	const command = client.commands.get(interaction.commandName);
 
-    if (!command) return;
+	if (!command) return;
 
-    try {
-        await command.default.execute(interaction, client);
-    } catch (error) {
-        console.error(error);
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-    }
-})
+	try {
+		await command.default.execute(interaction, client);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
+	}
+});
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
--- commands/css.js
+++ commands/css.js
@@ -1,34 +1,25 @@
-import { SlashCommandBuilder } from '@discordjs/builders'
+import { SlashCommandBuilder } from "@discordjs/builders";
 
 const css = {
-    data: new SlashCommandBuilder()
-        .setName("css")
-        .setDescription("Documentacion de CSS")
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('flexbox')
-                .setDescription('Información de Flexbox'))
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('media_query')
-                .setDescription('Informacion de media query'))
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('bootstrap')
-                .setDescription('Informacion de Bootstrap')),
-    async execute(interaction, client) {
-        const canal = await client.channels.fetch(process.env.GENERAL_CHANNEL)
-        if (interaction.options.getSubcommand() === "flexbox") {
-            await interaction.reply('Esta es toda la información de "Flexbox" de la que dispongo')
-            await canal.send("Flex")
-        } else if (interaction.options.getSubcommand() === "media_query") {
-            await interaction.reply('Esta es toda la información de "Media Query" de la que dispongo')
-            await canal.send("@query")
-        } else if (interaction.options.getSubcommand() === "bootstrap") {
-            await interaction.reply('Esta es toda la información de "Bootstrap" de la que dispongo')
-            await canal.send("Bootstrap")
-        }
-    }
-}
+	data: new SlashCommandBuilder()
+		.setName("css")
+		.setDescription("Documentacion de CSS")
+		.addSubcommand(subcommand => subcommand.setName("flexbox").setDescription("Información de Flexbox"))
+		.addSubcommand(subcommand => subcommand.setName("media_query").setDescription("Informacion de media query"))
+		.addSubcommand(subcommand => subcommand.setName("bootstrap").setDescription("Informacion de Bootstrap")),
+	async execute(interaction, client) {
+		const canal = await client.channels.fetch(process.env.GENERAL_CHANNEL);
+		if (interaction.options.getSubcommand() === "flexbox") {
+			await interaction.reply('Esta es toda la información de "Flexbox" de la que dispongo');
+			await canal.send("Flex");
+		} else if (interaction.options.getSubcommand() === "media_query") {
+			await interaction.reply('Esta es toda la información de "Media Query" de la que dispongo');
+			await canal.send("@query");
+		} else if (interaction.options.getSubcommand() === "bootstrap") {
+			await interaction.reply('Esta es toda la información de "Bootstrap" de la que dispongo');
+			await canal.send("Bootstrap");
+		}
+	},
+};
 
 export default css;
